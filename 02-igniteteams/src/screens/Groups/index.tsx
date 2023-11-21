d9e445f56a1573fd3cdcd928db11a457e788242b
Helder Cambuta <heldercambuta44@gmail.com>
2023-11-21 02:21:32 +0100

    :rocket: feat: Project 2 - Ignite Teams (The home page UI is done, now, I start coding the New Group page UI)

---
+++ 02-igniteteams/src/screens/Groups/index.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
+import { Container } from "./styles";
+
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
+
+export function Groups() {
+  const [groups, setGroups] = useState([]);
+
+  return (
+    <Container>
+      <Header />
+
+      <Highlight title="Turmas" subtitle="Jogue com suas turmas" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        ListEmptyComponent={
+          <ListEmpty message="Que tal Cadastrar a primeira Turma?" />
+        }
+      />
+
+      <Button title="Criar nova turma" />
+    </Container>
+  );
+}
